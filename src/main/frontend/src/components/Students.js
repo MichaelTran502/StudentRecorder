460bf12d509ff149c196189c781ce62660ad8ff5
Michael Tran <michael.tran2208@gmail.com>
2021-02-01 17:21:52 +1100

    added Ant Design styles

--- src/main/frontend/src/components/Students.js
+++ src/main/frontend/src/components/Students.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import { Table, Avatar } from 'antd';
 
 const Students = ({students}) => {
 
-  // Return all student details
+  const columns = [
+    {
+      title: '',
+      key: 'avatar',
+      render: (text, student) => (
+        <Avatar size='large'>
+          {`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+        </Avatar>
+      )
+    },
+    {
+      title: 'Student Id',
+      dataIndex: 'studentId',
+      key: 'studentId',
+    },
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      key: 'age',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'gender',
+    }
+  ];
+  
 
   return (
     <div>
-      {students.map(student => 
-        <div key={student.studentId}>
-          {student.firstName}
-        </div>
-        )}
+      <Table 
+      dataSource={students} 
+      columns={columns} 
+      rowKey='studentId'
+      pagination={false}
+      style = {{width: '1400px', margin: '0 auto'}}
+      />;
     
     </div>
   )
