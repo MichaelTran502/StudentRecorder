dc025f163c395eeb48d18700b4714695b3793564
Michael Tran <michael.tran2208@gmail.com>
2021-02-01 15:41:20 +1100

    added students endpoint

---
+++ src/main/java/com/tran/FullStack/controllers/StudentController.java
@@ -0,0 +1,30 @@
+package com.tran.FullStack.controllers;
+
+import com.tran.FullStack.models.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path="students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(UUID.randomUUID(),
+                        "James",
+                        "Bond",
+                        "jamesbond@gmail.com",
+                        Student.Gender.MALE),
+                new Student(UUID.randomUUID(),
+                        "Elisa",
+                        "Tamara",
+                        "elisatamara@gmail.com",
+                        Student.Gender.FEMALE)
+        );
+    }
+}
